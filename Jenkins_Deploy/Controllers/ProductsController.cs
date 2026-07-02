4b5d4cfdbbec7b7510a4f6803079946aa8ad6cab
yogarajan <yogaraja.g@gmail.com>
2026-07-02 21:10:02 +0530

    new product added-harddisc

--- Jenkins_Deploy/Controllers/ProductsController.cs
+++ Jenkins_Deploy/Controllers/ProductsController.cs
@@ -21,7 +21,8 @@ namespace Jenkins_Deploy.Controllers
             new Product { Id = 1, Name = "Keyboard", Price = 25.50m },
             new Product { Id = 2, Name = "Mouse", Price = 15.00m },
             new Product { Id = 3, Name = "Monitor", Price = 75.00m },
-            new Product { Id = 4, Name = "RAM", Price = 35.00m }
+            new Product { Id = 4, Name = "RAM", Price = 35.00m },
+            new Product { Id = 5, Name = "Hard Disc", Price = 105.00m }
         };
 
         public ProductsController(ILogger<ProductsController> logger)
